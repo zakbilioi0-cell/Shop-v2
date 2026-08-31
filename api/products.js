f30df548baccb31db5eb86a1443523cb1cde7d02
zakbilioi0-cell <zakbilioi0@gmail.com>
2026-08-31 17:40:20 +0700

    Create products.js

---
+++ api/products.js
@@ -0,0 +1,32 @@
+const PRODUCTS = {
+  'gayo-arabika': {
+    name: 'Gayo Arabika',
+    origin: 'Aceh, 1.200 mdpl',
+    note: 'Karamel, jeruk sitrun, akhir yang bersih',
+    price: 85000,
+  },
+  'toraja-arabika': {
+    name: 'Toraja Arabika',
+    origin: 'Sulawesi Selatan, 1.500 mdpl',
+    note: 'Rempah, tanah basah, body tebal',
+    price: 95000,
+  },
+  'flores-bajawa': {
+    name: 'Flores Bajawa',
+    origin: 'Nusa Tenggara Timur, 1.300 mdpl',
+    note: 'Cokelat gelap, karamel gosong',
+    price: 90000,
+  },
+  'mandheling': {
+    name: 'Mandheling',
+    origin: 'Sumatera Utara, 1.100 mdpl',
+    note: 'Herbal, kayu manis, low acidity',
+    price: 80000,
+  },
+};
+
+module.exports = (req, res) => {
+  res.status(200).json(PRODUCTS);
+};
+
+module.exports.PRODUCTS = PRODUCTS;
